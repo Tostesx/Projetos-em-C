1a64e9d51b84f3c7856d85f21408d2f9b891629b
Matheus Tostes <130178773+Tostesx@users.noreply.github.com>
2024-08-11 20:35:11 -0300

    Sistemas de vendas e devolução de troco ao cliente (Geladinho)
    First

---
+++ Prokk/main.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+    float geladinho = 5.00;
+    float qtd;
+    float valor;
+    float trococ;
+    float trocod;
+
+    while (qtd != 0) {
+        printf("Digite a quantidade do geladinho\n");
+        scanf("%f", &qtd);
+
+        valor = geladinho * qtd;
+        printf("Custo para o cliente: R$%.2f\n", valor);
+
+        printf("Digite o dinheiro do cliente\n");
+        scanf("%f", &trococ);
+
+        trocod = trococ - valor;
+
+        printf("Quanto custou para o cliente: R$%.2f\n", valor);
+        printf("Troco a ser entregue ao cliente: R$%.2f\n", trocod);
+    }
+
+    return 0;
+}
